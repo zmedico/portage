cda637a0baf64e5932aa854e46244e18122fc41c
Rin Cat <me@rin.cat>
2020-07-19 07:16:44 -0400

    Raise InvalidBinaryPackageFormat instead of return

--- lib/portage/dbapi/bintree.py
+++ lib/portage/dbapi/bintree.py
@@ -158,7 +158,8 @@ class bindbapi(fakedbapi):
 				metadata_bytes = portage.gpkg.gpkg(self.settings, mycpv,
 					binpkg_path).get_metadata()
 			else:
-				return
+				raise InvalidBinaryPackageFormat(
+					"Unknown binary package format %s" % binpkg_path)
 
 			def getitem(k):
 				if k == "_mtime_":
@@ -220,7 +221,8 @@ class bindbapi(fakedbapi):
 			mydata = mybinpkg.get_metadata()
 			encoding_key = False
 		else:
-			return
+			raise InvalidBinaryPackageFormat("Unknown binary package format %s"
+				% binpkg_path)
 
 		for k, v in values.items():
 			if encoding_key:
@@ -238,7 +240,8 @@ class bindbapi(fakedbapi):
 		elif binpkg_path.endswith(SUPPORTED_GPKG_EXTENSIONS):
 			mybinpkg.update_metadata(mydata)
 		else:
-			return
+			raise InvalidBinaryPackageFormat("Unknown binary package format %s"
+				% binpkg_path)
 
 		# inject will clear stale caches via cpv_inject.
 		self.bintree.inject(cpv, filename=binpkg_path)
